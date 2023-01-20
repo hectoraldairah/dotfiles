0f17ef1358447fd454fc074c1a6cc9c5e17d4800
hectoraldairah <hectoraldairah@protonmail.com>
2023-01-20 12:31:11 -0600

    added kitty config, spacebar and sketchybar

---
+++ colors.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Color Palette -- Catppuccin Macchiato
+BLACK=0xff181926
+WHITE=0xffcad3f5
+RED=0xffed8796
+GREEN=0xffa6da95
+BLUE=0xff8aadf4
+YELLOW=0xffeed49f
+ORANGE=0xfff5a97f
+MAGENTA=0xffc6a0f6
+GREY=0xff939ab7
+SKY=0xff91d7e3
+PINK=0xfff5bde6
+TRANSPARENT=0x00000000
+ITEM_COLOR=0xff1e1e2e
+
